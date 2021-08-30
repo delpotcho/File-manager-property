6cfb7e5092a84228fdb51b6dada73ed4ff83a28a
Nour Mohammed <delpotcho@gmail.com>
2021-08-30 18:34:57 +0200

    add swagger v2

--- src/main/java/com/sqli/stage/propertyfilemanager/controller/FileController.java
+++ src/main/java/com/sqli/stage/propertyfilemanager/controller/FileController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,7 +19,7 @@ import com.sqli.stage.propertyfilemanager.facade.ComparePropertieFileFacade;
 import com.sqli.stage.propertyfilemanager.service.FileService;
 
 import io.swagger.annotations.ApiOperation;
-
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/api")
 public class FileController {
@@ -27,7 +28,6 @@ public class FileController {
 
 	@Autowired
 	private ComparePropertieFileFacade comparePropertieFileFacade;
-	private File folder;
 
 	@PostMapping("/uploadFileZip")
 	@ApiOperation(value = " upload file zip  "
@@ -37,29 +37,23 @@ public class FileController {
 		String fileName = file.getOriginalFilename().toUpperCase();
 		try {
 			if (fileName.endsWith(".ZIP")) {
-				folder = comparePropertieFileFacade.comparePropertieFile(file);
+				 File  folder = comparePropertieFileFacade.comparePropertieFile(file);
 
-				return new ResponseEntity<>("fichier bien ajoute", HttpStatus.OK);
+				return new ResponseEntity<>(folder.getId(), HttpStatus.OK);
 
 			} else {
 				return new ResponseEntity<>("Ajouter fichier sous la forme '.zip' ", HttpStatus.METHOD_NOT_ALLOWED);
 			}
 
 		} catch (Exception e) {
-			return new ResponseEntity<>(HttpStatus.NOT_ACCEPTABLE);
+			return new ResponseEntity<Object>(HttpStatus.NOT_ACCEPTABLE);
 		}
 
 	}
 
-	@GetMapping("/getfile")
-	@ApiOperation(value = "look last  spesific file "
-				, notes = ""
-				, response = File.class)
-	public Optional<File> getlastFile() {
-		return fileService.getFileById(folder.getId());
-	}
+	
 
-	@GetMapping("/getfile/{id}")
+	@GetMapping("/file/{id}")
 	@ApiOperation(value = "finds file by id "
 				, notes = "provide an id to look up compare spesific file"
 				, response = File.class)
@@ -67,5 +61,5 @@ public class FileController {
 		return fileService.getFileById(id);
 
 	}
-
+	
 }
